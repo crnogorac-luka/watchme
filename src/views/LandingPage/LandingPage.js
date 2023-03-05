051bd2b805cb868761c10656d28ae512f6a418d4
Luka <lc4802@g.rit.edu>
2023-03-05 02:15:02 +0100

    project skeleton updated, new folders, files added

---
+++ src/views/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is the landing page.</p>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default LandingPage;
